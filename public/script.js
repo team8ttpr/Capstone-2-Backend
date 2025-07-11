2ed3c926a1d4224fc90ea90c7bfa325ee5b358fc
Finn Terdal <finnterdal@gmail.com>
2025-07-11 01:41:39 -0400

    edit index.html

--- public/script.js
+++ public/script.js
@@ -1,7 +1,14 @@
 const hostname = window.location.hostname;
 const protocol = window.location.protocol;
+const port = window.location.port;
+let portString = "";
+if (hostname === "localhost") {
+  portString = `:${port}`;
+} else {
+  portString = "";
+}
 
 const testDbLink = document.getElementById("test-db-link");
 
-testDbLink.href = `${protocol}//${hostname}:8080/api/test-db`;
-testDbLink.textContent = `${protocol}//${hostname}:8080/api/test-db`;
+testDbLink.href = `${protocol}//${hostname}${portString}/api/test-db`;
+testDbLink.textContent = `${protocol}//${hostname}${portString}/api/test-db`;
