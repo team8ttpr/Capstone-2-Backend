a9c449f433e9973dc7e773a7a59d25bdeb51d87a
Franccesco Petta <franccescopettamuller@gmail.com>
2025-07-31 14:54:14 -0400

    g

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const testDbRouter = require("./test-db");
+const app = require ("../app");
 
 router.use("/test-db", testDbRouter);
 
-module.exports = router;
+module.exports = {router, app};
