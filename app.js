58145588900e29064e4f7efe4f0298ecaadeb240
Franccesco Petta <franccescopettamuller@gmail.com>
2025-08-13 18:46:41 -0400

    cors

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const corsOptions = {
   origin: [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://capstone-2-frontend-one.vercel.app",
+    "https://capstone-2-frontend-tan.vercel.app",
   ],
   credentials: true,
   methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
